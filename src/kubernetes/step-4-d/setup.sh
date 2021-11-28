1cd24f316ad9f07042422b02ceb683a6f1f3d7f3
Thomas Brunko <thomas.brunko@gmail.com>
2021-11-28 13:38:21 +0100

    Added step-4, removed the substeps.

--- src/kubernetes/step-4-d/setup.sh
+++
@@ -1,5 +0,0 @@
-COMPONENT=step-4
-source ../../../setup.sh
-export KUBERNETES_NAMESPACE="${KUBERNETES_BASE_NAMESPACE}"
-
-export PS1='\[\e]0;\[\033[00;34m\]$(nice_path "\w")\[\033[00m\] $(if [[ $? == 0 ]]; then echo -en "\[\033[00;32m\]✓\[\033[00m\]"; else echo -en "\[\033[00;31m\]✗\[\033[00m\]"; fi) \$ '
